5eedbcbffbd8fe5c1ef1276a1828a5901454ca78
Francinah20 <118854529+Francinah20@users.noreply.github.com>
2024-06-16 00:06:39 +0200

    update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,39 +1,25 @@
 #!/usr/bin/python3
+'''
+A script to export data in the JSON format
+'''
 import json
 import requests
 
-def fetch_data():
-    # Fetch users and todos data
-    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-
-    return users, todos
-
-def main():
-    users, todos = fetch_data()
-
-    # Create a dictionary to hold all tasks for all employees
-    all_tasks = {}
-
-    # Create a dictionary of users for easy lookup
-    users_dict = {user['id']: user['username'] for user in users}
-
-    for todo in todos:
-        user_id = todo['userId']
-        if user_id not in all_tasks:
-            all_tasks[user_id] = []
-
-        task = {
-            "username": users_dict[user_id],
-            "task": todo['title'],
-            "completed": todo['completed']
-        }
-        all_tasks[user_id].append(task)
-
-    # Write the data to a JSON file
-    with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(all_tasks, json_file)
-
-if __name__ == "__main__":
-    main()
-
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users"
+    us = requests.get(url, verify=False).json()
+    undoc = {}
+    udoc = {}
+    for user in us:
+        uid = user.get("id")
+        udoc[uid] = []
+        undoc[uid] = user.get("username")
+    url = "https://jsonplaceholder.typicode.com/todos"
+    todo = requests.get(url, verify=False).json()
+    [udoc.get(t.get("userId")).append({"task": t.get("title"),
+                                       "completed": t.get("completed"),
+                                       "username": undoc.get(
+                                               t.get("userId"))})
+     for t in todo]
+    with open("todo_all_employees.json", 'w') as jsf:
+        json.dump(udoc, jsf)
