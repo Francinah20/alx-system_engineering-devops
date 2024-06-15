c14d01822fd160bd3e9e3cebf1acce3aa44eb0b7
Francinah20 <118854529+Francinah20@users.noreply.github.com>
2024-06-16 01:07:14 +0200

    hot fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,2 @@
-exec { 'change-os-configuration-for-holberton-user':
-  command => '/bin/echo "holberton soft nofile 4096" >> /etc/security/limits.conf && /bin/echo "holberton hard nofile 4096" >> /etc/security/limits.conf && /bin/echo "session required pam_limits.so" >> /etc/pam.d/common-session',
-  path    => ['/bin', '/usr/bin'],
-  unless  => '/bin/grep -q "holberton soft nofile 4096" /etc/security/limits.conf',
-}
-
+# Changes the OS configuration so that it is possible to login with the holberton user and open a file without any error message
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
