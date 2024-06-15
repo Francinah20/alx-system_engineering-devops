7715cfc4b66bab1cb69bd5249dde763979e1be53
Francinah20 <118854529+Francinah20@users.noreply.github.com>
2024-06-15 23:58:28 +0200

    HOT fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-'''
-Python script that returns information using REST API
-'''
+# Write a Python script that, using this REST API, for a given
+# employee ID, returns information about his/her Todo list progress.
+
 import requests
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        user = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-        req = requests.get("{}users/{}".format(url, user))
-        name = req.json().get("name")
-        if name is not None:
-            jreq = requests.get(
-                "{}todos?userId={}".format(
-                    url, user)).json()
-            alltsk = len(jreq)
-            completedtsk = []
-            for t in jreq:
-                if t.get("completed") is True:
-                    completedtsk.append(t)
-            count = len(completedtsk)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, count, alltsk))
-            for title in completedtsk:
-                print("\t {}".format(title.get("title")))
+    # Check if the script is provided with an employee ID as a command-line argument
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    employee_ID = sys.argv[1]
+    jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
+    url = f'{jsonplaceholder}/{employee_ID}'
+
+    # Make a GET request to the API
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        employee_name = response.json().get('name')
+        Todourl = f'{url}/todos'
+        res = requests.get(Todourl)
+        tasks = res.json()
+
+        # Filter completed tasks
+        done_tasks = [task for task in tasks if task.get('completed')]
+
+        # Display the employee TODO list progress
+        print("Employee {} is done with tasks({}/{}):".format(employee_name, len(done_tasks), len(tasks)))
+        for task in done_tasks:
+            print("\t{}".format(task.get('title')))
+    else:
+        # Display an error message if the request was not successful
+        print(f"Error: Unable to fetch data. Status code: {response.status_code}")
